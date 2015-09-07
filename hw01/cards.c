d2bdfcc20b06fd9317f055a3258c62f1509f65a7
jaazpa3 <jaazpa3@gmail.com>
2015-09-07 17:13:07 -0400

    adding 3 functions

--- hw01/cards.c
+++ hw01/cards.c
@@ -6,15 +6,16 @@ September 7, 2015
 
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
-	char card_name[3];
-	int count = 0;
-	while (card_name[0]!='X') {
-		puts("Enter the card_name: ");
-		scanf("%2s", card_name);
-		int val = 0;
-		switch(card_name[0]) {
+
+void *ask_user(char *card_name){
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	return 0;
+}
+
+int decide_value(char card_name[]){
+	int val;
+	switch(card_name[0]) {
 		case 'K':
 		case 'Q':
 		case 'J':
@@ -24,19 +25,31 @@ int main()
 			val = 11;
 			break;
 		case 'X':
-			continue;
 		default:
 			val = atoi(card_name);
 			if ((val < 1) || (val > 10)){
 				puts("I don't understand that value");
-				continue;
 			}
 		}
-		if ((val > 2) && (val < 7)) {
-			count++;
-		} else if (val == 10) {
-			count--;
-		}
+	return val;
+};
+
+int counter(int val){
+	int count = 0;	
+	if ((val > 2) && (val < 7)) {
+		count++;
+	} else if (val == 10) {
+		count--;
+	}
+	return count;	
+}
+
+int main() {
+	char card_name[3];
+	while (card_name[0]!='X') {
+		ask_user(card_name);
+		int val = decide_value(card_name);
+		int count = counter(val);
 		printf("Current count: %i\n", count);
 	}
 	return 0;

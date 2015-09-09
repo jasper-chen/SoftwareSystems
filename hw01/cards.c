34bc9f8b13c9c47dd3373673ef6208416ca7ac9f
jaazpa3 <jaazpa3@gmail.com>
2015-09-09 19:05:42 -0400

    adding ANSI-compliant code

--- hw01/cards.c
+++ hw01/cards.c
@@ -7,11 +7,20 @@ September 7, 2015
 #include <stdio.h>
 #include <stdlib.h>
 
-void *ask_user(char *card_name){
+int count = 0;
+
+/* Prompts user for input and puts the reply in the buffer.
+	card_name: buffer where result is stored
+*/	
+
+void ask_user(char *card_name){
 	puts("Enter the card_name: ");
 	scanf("%2s", card_name);
-	return 0;
+/*	return 0;*/
 }
+
+/* Maps card value on user's input 
+	card_name: array of chars on user's input */
 
 int decide_value(char card_name[]){
 	int val;
@@ -24,18 +33,19 @@ int decide_value(char card_name[]){
 		case 'A':
 			val = 11;
 			break;
-		case 'X':
 		default:
 			val = atoi(card_name);
 			if ((val < 1) || (val > 10)){
 				puts("I don't understand that value");
 			}
-		}
+	}
 	return val;
-};
+}
 
-int counter(int val){
-	int count = 0;	
+/* Changes count value based on card value
+	val: card value */
+
+int counter(int val){	
 	if ((val > 2) && (val < 7)) {
 		count++;
 	} else if (val == 10) {
@@ -46,10 +56,11 @@ int counter(int val){
 
 int main() {
 	char card_name[3];
+	int val;
 	while (card_name[0]!='X') {
 		ask_user(card_name);
-		int val = decide_value(card_name);
-		int count = counter(val);
+		val = decide_value(card_name);
+		counter(val);
 		printf("Current count: %i\n", count);
 	}
 	return 0;
